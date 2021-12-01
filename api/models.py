123c598fde8f193d0dd945ed9f46b5dec6afc20a
user.name <user.email>
2021-12-01 19:57:04 +0900

    외부 api 관련

--- api/models.py
+++ api/models.py
@@ -1,9 +1,17 @@
+from codecs import IncrementalDecoder
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
-class api(models.Model):
-    api=1;
-    order=1;
+class api_keys(models.Model):
+    api_code=models.IntegerField(primary_key=True)
+    api_name=models.CharField(max_length=20)
+    
+class api_key(models.Model):
+    api_code=models.ForeignKey("api_code", related_name="code")
+    api_order=models.AutoField(primary_key="True")
+    key=models.CharField(max_length=50)
+    
     
 
     
